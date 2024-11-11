cec7df90ebe672795df294de7211c3e8323be3b3
burakloklu <burakloklu@gmail.com>
2024-11-11 15:29:10 +0300

    Divider center bug fix

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion';
 
 export default function SectionDivider() {
   return (
-    <div className='w-[100%] justify-items-center'>
+    <div className='min-w-[100%] justify-items-center'>
       <motion.div
-        className="dark:bg-[#ffffff33] bg-gray-300 sm:bg-gray-200  mt-12 mb-32 h-12 w-1 rounded-full sm:block sm:my-32"
+        className="dark:bg-[#ffffff33] bg-gray-300 sm:bg-gray-200  mt-12 mb-32 h-12 w-1 rounded-full sm:block sm:my-32 mr-auto ml-auto"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.125 }}
