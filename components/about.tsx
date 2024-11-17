23c7533fa2115af27e913b76af348adabf23fd5b
burakloklu <burakloklu@gmail.com>
2024-11-17 14:08:42 +0300

    Update resume

--- components/about.tsx
+++ components/about.tsx
@@ -53,8 +53,8 @@ export default function About() {
             <div className="flex flex-row gap-4 max-[639px]:justify-center mt-10 h-12">
               <a
                 className="group flex items-center whitespace-nowrap max-w-[15rem] sm:max-w-[13rem] font-medium sm:text-sm bg-[#f9fafc] px-7 py-3 gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-                href="/Burak_Loklu_Resume.pdf"
-                download="Burak_Loklu_Resume.pdf"
+                href="/burakloklu_resume.pdf"
+                download="burakloklu_resume.pdf"
                 onClick={() => sendGAEvent('event', 'buttonClicked', { event_category: 'Download', event_label: 'Resume Download' })}
               >
                 Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
